bd929b3eb18b45cea945dab8ec76ad8546d7cf0d
Lucas-Jacobsen <97192501+Lucas-Jacobsen@users.noreply.github.com>
2023-09-07 19:36:56 -0700

    Add files via upload

---
+++ src/shuttle/shuttle.dao.ts
@@ -0,0 +1,37 @@
+import { OkPacket } from "mysql";
+import { execute } from "../services/mysqlconnector";
+import { Shuttle } from "./shuttle.model";
+import {shuttleQueries} from './shuttle.queries';
+
+export const readShuttles = async() => 
+{
+    console.log("In readShuttles DAO");
+
+    return execute<Shuttle[]>(shuttleQueries.readShuttles, [])
+};
+
+export function readShuttlesByShuttleId(shuttleId: number) : any {
+    console.log("In readShuttlesByShuttleId DAO");
+    return execute<Shuttle[]>(shuttleQueries.readShuttlesByShuttleId, [shuttleId]);
+}
+
+export const createShuttle =async (shuttle : Shuttle) => 
+{
+    console.log("In createShuttle DAO");
+
+    return execute<OkPacket>(shuttleQueries.createShuttle, [shuttle.id, shuttle.code, shuttle.lat, shuttle.long, shuttle.status, shuttle.type])
+};
+
+export function updateShuttle(shuttle: Shuttle): OkPacket | PromiseLike<OkPacket> 
+{
+    console.log("In updateShuttle DAO");
+    return execute<OkPacket>(shuttleQueries.updateShuttle, [shuttle.id, shuttle.code, shuttle.lat, shuttle.long, shuttle.status, shuttle.type])
+
+};
+
+export function deleteShuttle(shuttleId: number)
+{
+    console.log("In deleteShuttle DAO");
+
+    return execute<OkPacket>(shuttleQueries.deleteShuttle, [shuttleId])
+};
---
+++ src/trip/trip.dao.ts
@@ -0,0 +1,36 @@
+import { OkPacket } from "mysql";
+import { execute } from "../services/mysqlconnector";
+import { Trip } from "./trip.model";
+import {tripQueries} from './trip.queries';
+
+export const readTrip = async() => 
+{
+    console.log("In readTrips DAO");
+
+    return execute<Trip[]>(tripQueries.readTrips, [])
+};
+
+export function readTripsByTripId(tripId: number) : any {
+    console.log("In readTripssByTripsId DAO");
+    return execute<Trip[]>(tripQueries.readTripsByTripId, [tripId]);
+}
+
+export const createTrip =async (trip : Trip) => 
+{
+    console.log("In createTrip DAO");
+
+    return execute<OkPacket>(tripQueries.createTrip, [trip.id, trip.shuttle, trip.pickup, trip.dropoff, trip.pax, trip.dur])
+};
+
+export function updateTrip(trip: Trip): OkPacket | PromiseLike<OkPacket> 
+{
+    console.log("In updateTrip DAO");
+    return execute<OkPacket>(tripQueries.updateTrip, [trip.id, trip.shuttle, trip.pickup, trip.dropoff, trip.pax, trip.dur])
+};
+
+export function deleteShuttle(tripId: number)
+{
+    console.log("In deleteTrip DAO");
+
+    return execute<OkPacket>(tripQueries.deleteTrip, [tripId])
+};
